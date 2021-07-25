1e72c570fb3f0cf4c5add6c4dd85be0df055ceb3
Jyotik Mayur <heyjyotik@gmail.com>
2021-07-25 12:04:51 +0100

    Fixed the connection issue

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -10,26 +10,26 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val p1Name: EditText = findViewById(R.id.p1_name)
+        val p2Name: EditText = findViewById(R.id.p2_name)
+        val p1TurnLabel: TextView = findViewById(R.id.p1)
+        val p2TurnLabel: TextView = findViewById(R.id.p2)
+
+
+        //First Row Buttons
+        val zeroZero: Button = findViewById(R.id.one_one)
+        val zeroOne: Button = findViewById(R.id.one_two)
+        val zeroTwo: Button = findViewById(R.id.one_three)
+
+        //Second Row Buttons
+        val oneZero: Button = findViewById(R.id.two_one)
+        val oneOne: Button = findViewById(R.id.two_two)
+        val oneTwo: Button = findViewById(R.id.two_three)
+
+        //Third Row Buttons
+        val threeZero: Button = findViewById(R.id.three_one)
+        val threeOne: Button = findViewById(R.id.three_two)
+        val threeTwo: Button = findViewById(R.id.three_three)
     }
-
-    val p1Name: EditText = findViewById(R.id.p1_name)
-    val p2Name: EditText = findViewById(R.id.p2_name)
-    val p1TurnLabel: TextView = findViewById(R.id.p1)
-    val p2TurnLabel: TextView = findViewById(R.id.p2)
-
-
-    //First Row Buttons
-    val zeroZero: Button = findViewById(R.id.one_one)
-    val zeroOne: Button = findViewById(R.id.one_two)
-    val zeroTwo: Button = findViewById(R.id.one_three)
-
-    //Second Row Buttons
-    val oneZero: Button = findViewById(R.id.two_one)
-    val oneOne: Button = findViewById(R.id.two_two)
-    val oneTwo: Button = findViewById(R.id.two_three)
-
-    //Third Row Buttons
-    val threeZero: Button = findViewById(R.id.three_one)
-    val threeOne: Button = findViewById(R.id.three_two)
-    val threeTwo: Button = findViewById(R.id.three_three)
 }
